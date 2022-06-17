1b496b8e281680abd5a45aa09d60cc4301c05736
yoon54jh <yoonjh00@hs.ac.kr>
2022-06-17 12:14:17 +0900

    Initial commit

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3xv(m23e43_m-_w^2u4_r*g1f=!prth419%#^*%f61cx^o&utz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
